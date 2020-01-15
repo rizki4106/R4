e28be131932cca651bdc99b9f2afd9e213bdf99f
rizki4106 <rizkimaulana4106@gmail.com>
2020-01-16 05:39:31 +0700

    adding the author

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ def readme():
 setup(
     name="R4",
     version="0.0.1",
+    author_email="rizkimaulana348@gmail.com",
     description="R4 is a python package for encrypt your important data",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/rizki4106/R4.git",
+    download_url="https://github.com/rizki4106/R4/archive/master.zip",
     keywords=["encrypt", "decrypt", "base64", "base64_encode", "base64_decode","md5"],
     license="MIT",
     classifiers=[
